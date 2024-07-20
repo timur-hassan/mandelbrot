d3adff50b1d95feacdc1decf30e6d8814cd73647
timur-hassan <timur.hassan@gmail.com>
2024-07-20 19:00:37 +1000

    fixed numpy "warnings" deprecation

--- 01_scatter_plot.py
+++ 01_scatter_plot.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as np, warnings
 
+np.warnings = warnings
 np.warnings.filterwarnings("ignore")
 
 
 def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
-    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
-    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
-    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
+        re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+        im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
+        return re[np.newaxis, :] + im[:, np.newaxis] * 1j
 
 
 def is_stable(c, num_iterations):
